5005baca524304ffbfacd2163d65e09130f44d83
Jia Chiuan <b1129046@cgu.edu.tw>
2024-06-02 17:09:06 +0800

    chore: Update package dependencies for react-leaflet and leaflet

--- DataBase/client/src/Home.js
+++ DataBase/client/src/Home.js
@@ -1,10 +1,21 @@
-// Home.js
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './App.css'; // Import the CSS file
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import './App.css';
+
+// Set default icon for leaflet
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png',
+  iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
+});
 
 function Home() {
   const [searchTerm, setSearchTerm] = useState('');
+  const [locations, setLocations] = useState([]);
   const navigate = useNavigate();
 
   const handleSearchChange = (e) => {
@@ -15,6 +26,13 @@ function Home() {
     e.preventDefault();
     navigate(`/search?query=${searchTerm}`);
   };
+
+  useEffect(() => {
+    fetch('http://localhost:4001/api/locations/home')
+      .then(response => response.json())
+      .then(data => setLocations(data))
+      .catch(error => console.error('Error fetching locations:', error));
+  }, []);
 
   return (
     <div className="Home">
@@ -31,14 +49,25 @@ function Home() {
           <button type="submit">Search</button>
         </form>
       </header>
-      <div className="image-placeholder-container">
-        <div className="image-placeholder"></div>
-        <div className="image-placeholder"></div>
-        <div className="image-placeholder"></div>
-        <div className="image-placeholder"></div>
+      <div className="map-container">
+        <MapContainer center={[23.6978, 120.9605]} zoom={7} style={{ height: "600px", width: "100%" }}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          {locations.map(location => (
+            <Marker key={location.id} position={[location.latitude, location.longitude]}>
+              <Popup>
+                <b>{location.location_name}</b><br />{location.address}
+              </Popup>
+            </Marker>
+          ))}
+        </MapContainer>
       </div>
+      <div id="info"></div>
     </div>
   );
 }
+
 
 export default Home;

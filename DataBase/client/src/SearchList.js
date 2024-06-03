b35feb612d70a4033157edb0e2e627fc5a07da4d
Jia Chiuan <b1129046@cgu.edu.tw>
2024-06-04 00:25:48 +0800

    Update opening time label to display in Chinese

--- DataBase/client/src/SearchList.js
+++ DataBase/client/src/SearchList.js
@@ -67,7 +67,7 @@ function SearchList() {
           <div key={location.id} className="location-item">
             <span className="location-title">{location.location_name}</span>
             <div className="location-details">
-              <p>開幕時間: {location.opening_time}</p>
+              <p>開門時間: {location.opening_time}</p>
               <p>打烊時間: {location.closing_time}</p>
               <p>地址: {location.address}</p>
             </div>

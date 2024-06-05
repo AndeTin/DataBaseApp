ae8553e0e255297a5efc0da19e653608a8b5c8ff
Jia Chiuan <b1129046@cgu.edu.tw>
2024-06-05 23:29:20 +0800

    refactor: Add CORS middleware to server.js for handling cross-origin requests

--- DataBase/server/server.js
+++ DataBase/server/server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const mysql = require('mysql');
-const cors = require('cors');
+const cors = require('cors'); // Import the cors middleware
 const bodyParser = require('body-parser');
 
 const app = express();
 const port = 4001;
 
-app.use(cors());
+app.use(cors()); // Use CORS middleware to allow cross-origin requests
 app.use(express.json());
 app.use(bodyParser.json());
 
